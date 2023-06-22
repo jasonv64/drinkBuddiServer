4f4ba8a4cf74f2bab3ab4500a44fa57036d96ab0
jasonv64 <Valadezj045@gmail.com>
2023-06-21 21:23:27 -0700

    bcrypt proper set up

--- routes/user.js
+++ routes/user.js
@@ -4,9 +4,12 @@ const express = require("express");
 const dotenv = require('dotenv');
 const router = express.Router();
 const cloudinary = require('cloudinary');
+const { v4: uuidv4 } = require('uuid')
+var bcrypt = require('bcryptjs');
+
 dotenv.config({path:'../.env'});
 
-router.get('/api/user',(req,res,next)=>{
+router.get('/api/user:user_id',(req,res,next)=>{
     var db = connectDatabase();
     let sql =    
     `SELECT 
@@ -14,7 +17,7 @@ router.get('/api/user',(req,res,next)=>{
     FROM 
         USERS
     WHERE 
-        USER_ID='454121'`;
+        USER_ID='${req.params.user_id}'`;
 
     db.query(sql,(err,result)=>{
         if(err) throw res.status(400).send('err');
@@ -25,82 +28,79 @@ router.get('/api/user',(req,res,next)=>{
     db.end((err)=>{if (err) throw err;});
 });
 
-
 router.post('/api/user/signup',async (req,res) => {
-    var db  = create_connection();
     var data = req.body;
-    console.log(data)
+    let sql = 'INSERT INTO USERS SET ?';
+    var db  = connectDatabase();
 
-    if(!data.imageUploaded) {
-        console.log("use default")
-        data['profilePictureLink'] = 'https://res.cloudinary.com/dppkrg7h5/image/upload/v1679691231/not-sw/Screenshot_2023-03-24_135342_fdr6cp.png';
-        delete data.imageUploaded;
-    } else {
-        cloudinary.config({
-            cloud_name: process.env.cloudinaryCloudName,
-            api_key: process.env.cloudinaryAPIKEY,
-            api_secret: process.env.cloudinarySecret,
-            folder: 'not-sw'
-          });
-        
-        const uploadResponse = await cloudinary.uploader.upload(
-            data.profilePicture,
-            {
-                upload_preset:'dev_setups'
-            }
-        ).catch(res => (console.log(res, uploadResponse)));
-    
-        data['profilePictureLink'] = uploadResponse.secure_url;
-        delete data.profilePicture;
-        delete data.imageUploaded;
-    }
-
-    data['USER_ID'] = (uuidv4());
-    data['SUBSCRIPTION_ID '] = (uuidv4());
-    data['DATE_JOINED'] = new Date().toISOString().slice(0, 19).replace('T', ' ');
-    
-    let sql = 'INSERT INTO user SET ?';
-
-    db.query(sql, data, (err, result) => {
-        if(err) throw err;
-    });
-
-    db.end(function(err){
-        if (err) throw err;
-    });
-
-    res.send('Sign-up Successful!')
+    bcrypt.genSalt(10, function(err, salt) {
+        bcrypt.hash(req.body.PASSWORD, salt, function(err, hash) {
+            data["PASSWORD"] = hash;
+            data['USER_ID'] = (uuidv4());
+            data['SUBSCRIPTION_ID'] = (uuidv4());
+            data['DATE_JOINED'] = new Date().toISOString().slice(0, 19).replace('T', ' ');
+            
+            //retrives cloudinary URL and assigns it in data.
+            if(!data.imageUploaded) {
+                console.log("use default")
+                data['PICTURE'] = 'https://res.cloudinary.com/dppkrg7h5/image/upload/v1679691231/not-sw/Screenshot_2023-03-24_135342_fdr6cp.png';
+                delete data.imageUploaded;
+            } else {
+                cloudinary.config({
+                    cloud_name: process.env.cloudinaryCloudName,
+                    api_key: process.env.cloudinaryAPIKEY,
+                    api_secret: process.env.cloudinarySecret,
+                    folder: 'not-sw'
+                  });
+                
+                const uploadResponse = cloudinary.uploader.upload(
+                    data.profilePicture,
+                    {
+                        upload_preset:'dev_setups'
+                    }
+                ).catch(res => (console.log(res, uploadResponse)));
+            
+                data['PICTURE'] = uploadResponse.secure_url;
+                delete data.profilePicture;
+                delete data.imageUploaded;
+            }            
+            db.query(sql, data, (err, result) => {
+                if(err) throw err;
+            });
+            db.end(function(err){
+                if (err) throw err;
+            });
+        });
+    }); 
+    res.send({Message: 'Sign-up Successful!'})
 })
 
 router.post('/api/user/login', (req, res)=> {
-    var db = create_connection();
-    var user_email = req.body.email;
-    var user_attempted_password = req.body.password;
+    var db = connectDatabase();
+    var user_email = req.body.EMAIL;
     var user = {};
-    let sql = 'SELECT * FROM USER WHERE EMAIL = ?';
+
+    let sql = 'SELECT * FROM USERS WHERE EMAIL = ?';
 
     db.query(sql, user_email, (err, result) => {
+          console.log(req.body.PASSWORD, result[0].PASSWORD)
         if(result.length === 0) {
-            console.log('Email does not exist. Please sign up')
-            return res.send('Email does not exist. Please sign up')
+            return res.send({message: "Email does not exist. Please sign up"})
         }
-        if (err) throw err;
-        ///this line will be replaced with bycrypt
-        if (result[0].password === user_attempted_password) {
-            user = Object.assign(JSON.parse(JSON.stringify(result[0])),user);
 
-        } else {
-            console.log('Wrong password.')
-            return res.send('Wrong password.')           
-        }
+        if (err) throw err;
+        
+        bcrypt.compare(req.body.PASSWORD, result[0].PASSWORD, function(err, r) {
+            if (r) {
+                user = Object.assign(JSON.parse(JSON.stringify(result[0])),user);
+                console.log("we're in boys")
+            } else {
+                return res.send({message: "wrong password"}) 
+            }
+        });
+
+
     
-    let preferencesSql = `SELECT * FROM PREFERENCES WHERE USER_ID = '${user.user_id}'`;
-    db.query(preferencesSql,(err,result) => {
-        if(err) throw err;
-        user = Object.assign({preferences: JSON.parse(JSON.stringify(result)) },user);
-        console.log(user)
-        return res.send(user)
-    })
     db.end(function(err){
             if (err) throw err;
         });
